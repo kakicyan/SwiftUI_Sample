c003ad5561819d0b9be410dfb93e75d3dcedc4ed
kakicyan <kinoatkyoto@gmail.com>
2021-08-17 09:56:30 +0900

    sample

---
+++ SPMSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SPMSample
+//
+//  Created by 垣本耀介 on 2021/08/16.
+//
+
+import SwiftUI
+import Alamofire
+
+struct ContentView: View {
+    var body: some View {
+      Text("Hello, world!")
+         .foregroundColor(Color.blue)
+            .padding()
+         
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SPMSample/SPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMSampleApp.swift
+//  SPMSample
+//
+//  Created by 垣本耀介 on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct SPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
